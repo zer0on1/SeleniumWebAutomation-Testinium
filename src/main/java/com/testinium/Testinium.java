64f7633bdf3cf69965a74e8fe77c1592f8a8f315
zer0on1 <birahmetozdemir@gmail.com>
2020-02-29 21:33:26 +0300

    updated.

--- src/main/java/com/testinium/Testinium.java
+++ src/main/java/com/testinium/Testinium.java
@@ -32,10 +32,12 @@ public class Testinium {
         driver.findElement(By.className("searchBtn")).click();
         Thread.sleep(1000);
         driver.findElement(By.xpath("//div[@class='pagination']//a[contains(text(),'2')]")).click();
+        Thread.sleep(1000);
         Assert.assertEquals(driver.getCurrentUrl().split("=")[2], "2");
         List<WebElement> computers = driver.findElements(By.xpath("//body/div[@id='wrapper']/div[@id='contentListing']/div[@class='container']/div[@class='listingHolder']/div[@class='productArea']/section[@class='group listingGroup resultListGroup import-search-view']/div[@id='view']/ul[@class='clearfix']/li"));
         int random = (int) (Math.random() * computers.size() + 1);
         driver.findElement(By.xpath("/html[1]/body[1]/div[1]/div[2]/div[1]/div[1]/div[2]/section[1]/div[2]/ul[1]/li[" + random + "]/div[1]/div[1]/a[1]/img[1]")).click();
+        Thread.sleep(1000);
         driver.findElement(By.linkText("Sepete Ekle")).click();
         String price = driver.findElement(By.xpath("//div[@class='newPrice']")).getText().split("TL")[0];
         Thread.sleep(1000);
@@ -45,11 +47,11 @@ public class Testinium {
         Assert.assertEquals(price, basketPrice);
         int beforeIncreasingCount = Integer.parseInt(driver.findElement(By.className("quantity")).getAttribute("value"));
         driver.findElement(By.xpath("//span[@class='spinnerUp spinnerArrow']")).click();
-        Thread.sleep(1000);
+        Thread.sleep(3000);
         int afterIncreasingCount = Integer.parseInt(driver.findElement(By.className("quantity")).getAttribute("value"));
         Assert.assertEquals(beforeIncreasingCount + 1, afterIncreasingCount);
         driver.findElement(By.xpath("//span[@class='removeProd svgIcon svgIcon_trash']")).click();
-        Thread.sleep(1000);
+        Thread.sleep(5000);
         Assert.assertEquals(driver.findElement(By.xpath("/html[1]/body[1]/div[1]/div[2]/div[1]/div[1]/div[1]/h2[1]")).getText(), "Sepetiniz Boş");
     }
 }
