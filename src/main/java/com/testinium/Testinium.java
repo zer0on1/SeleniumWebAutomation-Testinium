83d0331bea70472cfedf78380ac8dd1453d497fb
zer0on1 <birahmetozdemir@gmail.com>
2020-02-29 22:06:22 +0300

    important information added.

--- src/main/java/com/testinium/Testinium.java
+++ src/main/java/com/testinium/Testinium.java
@@ -16,9 +16,11 @@ public class Testinium {
     @Before
     public void setUp() {
         System.setProperty("webdriver.chrome.driver", "C:\\Users\\Ahmet\\Desktop\\chromedriver_win32\\chromedriver.exe");
+        // you should write the your file path for "chromedriver.exe"
         driver = new ChromeDriver();
         driver.manage().window().maximize();
         driver.get("http://www.n11.com/");
+
     }
 
     @Test
